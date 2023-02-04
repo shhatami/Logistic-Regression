889e1d0a6dc56dc83f2c7ac35cd3323316f65452
Shani.ha <53007257+shhatami@users.noreply.github.com>
2023-02-04 11:20:30 +0330

    Create Data Pre-processing.py

---
+++ Data Pre-processing.py
@@ -0,0 +1,8 @@
+    #Data Pre-procesing Step  
+    # importing libraries  
+    import numpy as nm  
+    import matplotlib.pyplot as mtp  
+    import pandas as pd  
+      
+    #importing datasets  
+    data_set= pd.read_csv('user_data.csv')  
