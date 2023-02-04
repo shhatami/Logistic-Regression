44186b3f161b0d55b819f66acb25473923233786
Shani.ha <53007257+shhatami@users.noreply.github.com>
2023-02-04 11:27:27 +0330

    Create Output.the Training set.py

---
+++ Output.the Training set.py
@@ -0,0 +1,5 @@
+LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,  
+                   intercept_scaling=1, l1_ratio=None, max_iter=100,  
+                   multi_class='warn', n_jobs=None, penalty='l2',  
+                   random_state=0, solver='warn', tol=0.0001, verbose=0,  
+                   warm_start=False) 
