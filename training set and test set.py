675a2c70c3bea22057d24f6a18a350e566d0e4f6
Shani.ha <53007257+shhatami@users.noreply.github.com>
2023-02-04 11:22:14 +0330

    Create training set and test set.py

---
+++ training set and test set.py
@@ -0,0 +1,3 @@
+    # Splitting the dataset into training and test set.  
+    from sklearn.model_selection import train_test_split  
+    x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=0)  
