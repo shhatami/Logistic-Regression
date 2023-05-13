cef3bb129cc97afed3a54037c49fb18af7e214e0
Shani.ha <sh_hatami84@yahoo.com>
2023-05-13 07:15:04 +0330

    Update training set and test set.py

--- training set and test set.py
+++ training set and test set.py
@@ -1,3 +1,4 @@
     # Splitting the dataset into training and test set.  
     from sklearn.model_selection import train_test_split  
-    x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=0)  
+    
+    x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=0)
